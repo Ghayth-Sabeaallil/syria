479f81e1fec7465f2a4fdfaec25e8938d83dfcff
ghayth.sabeaallil <gs222mh@gmail.com>
2024-12-17 23:46:26 +0100

    bg image

--- src/Components/MapView.tsx
+++ src/Components/MapView.tsx
@@ -5,7 +5,7 @@ type ParentProps = {
 };
 const MapView = ({ children }: ParentProps) => {
     return (
-        <svg baseProfile="tiny" fill="grey" height="918" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.7" version="1.2" viewBox="0 0 1000 918" width="1000" xmlns="http://www.w3.org/2000/svg">
+        <svg baseProfile="tiny" fill="grey" height="99vh" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" version="1.2" viewBox="0 0 1000 918" width="100vw" xmlns="http://www.w3.org/2000/svg">
             <g id="features">
                 {children}
             </g>
