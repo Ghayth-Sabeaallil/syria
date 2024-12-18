aaf228d45b41855055a6a1aaa2ee1e4d214e1a83
ghayth.sabeaallil <gs222mh@gmail.com>
2024-12-18 23:50:23 +0100

    state hover

--- src/Components/MapView.tsx
+++ src/Components/MapView.tsx
@@ -5,10 +5,8 @@ type ParentProps = {
 };
 const MapView = ({ children }: ParentProps) => {
     return (
-        <svg baseProfile="tiny" fill="grey" height="99vh" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" version="1.2" viewBox="0 0 1000 918" width="100vw" xmlns="http://www.w3.org/2000/svg">
-            <g id="features">
-                {children}
-            </g>
+        <svg baseProfile="tiny" fill="grey" height="99vh" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" version="1.2" viewBox="10 10 1000 918" width="100vw" xmlns="http://www.w3.org/2000/svg">
+            {children}
         </svg>
     )
 }
--- src/Components/States/Damascus.tsx
+++ src/Components/States/Damascus.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import { StateProps } from "../../lib/types"
 
 const Damascus = ({ handleClick }: StateProps) => {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
-        <path onClick={handleClick} d="M126.5 689.6l-2.6-0.4-8.3-4.2-3.1 2-0.7-1.6 2.9-3.1-3.2-1.6-2.2 0.3-1-3 5.2-2.9 4.1-0.2 7.6-2.2 3.8 0.3 2.2 3-0.1 1.8 0.3 3-1.3 3.6-3.1 3.2-0.5 2z" id="SYDI" name="Damascus">
+        <path onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)} onClick={handleClick} style={isHovered ? { fill: "#666563", opacity: 0.75 } : {}} d="M126.5 689.6l-2.6-0.4-8.3-4.2-3.1 2-0.7-1.6 2.9-3.1-3.2-1.6-2.2 0.3-1-3 5.2-2.9 4.1-0.2 7.6-2.2 3.8 0.3 2.2 3-0.1 1.8 0.3 3-1.3 3.6-3.1 3.2-0.5 2z" id="SYDI" name="Damascus">
         </path>
     )
 }
