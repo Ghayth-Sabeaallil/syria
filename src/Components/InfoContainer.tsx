6493f1cd22a5a069875ee6a3410edd49b6520d32
ghayth.sabeaallil <gs222mh@gmail.com>
2025-01-05 01:14:42 +0100

    add some icons

--- src/Components/InfoContainer.tsx
+++ src/Components/InfoContainer.tsx
@@ -1,10 +1,17 @@
+import { useTranslation } from "react-i18next";
+import { FaMosque } from "react-icons/fa";
+import { GiForest } from "react-icons/gi";
+import { MdChurch } from "react-icons/md";
+import { PiCastleTurretFill } from "react-icons/pi";
 
 function InfoContainer() {
+    const { t } = useTranslation();
     return (
-        <div className="grid grid-cols-1 gap-2 sm:grid-cols-1 md:grid-cols-4 justify-center items-center m-auto h-screen">
-            <div className="grid grid-cols-[auto_1fr_auto] items-center h-full rounded-md">
-                <div className="flex justify-center items-center h-5/6 bg-white rounded-lg shadow-lg text-xl font-bold text-gray-800"></div>
-            </div>
+        <div className="grid grid-cols-1 sm:grid-cols-1 gap-4 md:grid-cols-4 m-auto h-auto p-4 justify-items-center">
+            <div className="flex flex-col gap-2 justify-center items-center bg-white rounded-lg shadow-lg w-6/12 h-[25vh] text-lg sm:text-lg md:text-3xl font-bold text-gray-800"><PiCastleTurretFill size={50} />{t('castles')}</div>
+            <div className="flex flex-col gap-2 justify-center items-center bg-white rounded-lg shadow-lg w-6/12 h-[25vh] text-lg sm:text-lg md:text-3xl font-bold text-gray-800"><FaMosque size={50} />{t('mosques')}</div>
+            <div className="flex flex-col gap-2 justify-center items-center bg-white rounded-lg shadow-lg w-6/12 h-[25vh] text-lg sm:text-lg md:text-3xl font-bold text-gray-800"><MdChurch size={50} />{t('churches')}</div>
+            <div className="flex flex-col gap-2 justify-center items-center bg-white rounded-lg shadow-lg w-6/12 h-[25vh] text-lg sm:text-lg md:text-3xl font-bold text-gray-800"><GiForest size={50} />{t('nature')}</div>
         </div>
 
     )
