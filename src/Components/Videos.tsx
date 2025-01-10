f455efcd4452054a905d40c20b103ebedc5a59fc
ghayth.sabeaallil <gs222mh@gmail.com>
2025-01-10 18:26:59 +0100

    videos

--- src/Components/Videos.tsx
+++ src/Components/Videos.tsx
@@ -1,13 +1,4 @@
-type VideosProps = {
-    title?: string;
-    icon?: string;
-    des?: string;
-    link?: string;
-};
-
 const Videos = () => {
-
-
     return (
         <div className="grid grid-cols-1 sm:grid-cols-1 gap-4 md:grid-cols-3 m-4 p-2 justify-items-center">
             <iframe className="w-full h-[35vh]" src="https://www.youtube.com/embed/m62ZGavdj-8?si=gC-VYTlms00-UikT" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
